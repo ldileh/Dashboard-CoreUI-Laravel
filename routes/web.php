54479e962a1cce8c9f6d1985d16f180f4672508d
ldileh <ldileh@gmail.com>
2018-09-08 10:56:31 +0700

    update web route

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,11 @@ Route::group([ 'prefix' => 'panel', 'middleware' => [ 'auth', 'auth.admin' ] ], 
 	Route::group([ 'prefix' => 'users' ], function() {
 	    Route::get('/', 'Panel\UserController@index')->name('user');
 	    Route::get('/data', 'Panel\UserController@getData')->name('user.data');
-	    Route::get('{userId}/delete', 'Panel\UserController@destroy')->name('user.data.delete');
 	    Route::get('create', 'Panel\UserController@create')->name('user.create');
 		Route::post('create', 'Panel\UserController@store')->name('user.data.create');
 		Route::get('{userId}/edit', 'Panel\UserController@edit')->name('user.edit');
 	    Route::post('{userId}/edit', 'Panel\UserController@update')->name('user.data.edit');
+	    Route::get('{userId}/delete', 'Panel\UserController@destroy')->name('user.data.delete');
 	});
 });
 
