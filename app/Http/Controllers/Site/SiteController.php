1ae042c976f52fff21aa99d20e9499a8ad6dcc84
ldileh <ldileh@gmail.com>
2022-02-24 21:37:15 +0700

    update source code

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -11,12 +11,9 @@ use App\Models\Video;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Str;
 use Carbon;
-use Markdown;
 
 class SiteController extends Controller
 {
