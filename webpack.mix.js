b10edad2afabcc55e24e5a8911ccc87af31002f5
ldileh <ldileh@gmail.com>
2022-03-13 00:22:44 +0700

    update some resource

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
 	.sass('resources/assets/sass/app.scss', 'public/css')
+	.sass('resources/assets/sass/style_site.scss', 'public/css')
 	.styles([
 		'resources/assets/css/font-awesome.min.css',
 		'resources/assets/css/coreui-icons.min.css',
