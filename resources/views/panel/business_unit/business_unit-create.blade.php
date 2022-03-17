d00b9401663f6084b9baa075d126250a73ee52b6
ldileh <ldileh@gmail.com>
2022-03-17 23:04:14 +0700

    fix function copy link image on page create & edit business unit

--- resources/views/panel/business_unit/business_unit-create.blade.php
+++ resources/views/panel/business_unit/business_unit-create.blade.php
@@ -246,16 +246,7 @@
         $('#data-table-post_image tbody').on('click', '.button-copy', function(){
             var value = $(this).data('value');
 
-            navigator.clipboard.writeText(value).then(
-                function() {
-                    /* clipboard successfully set */
-                    window.alert('Success! The text was copied to your clipboard')
-                },
-                function() {
-                    /* clipboard write failed */
-                    window.alert('Opps! Your browser does not support the Clipboard')
-                }
-            );
+            copyTextToClipboard(value);
         });
 
         $('#data-table-post_image tbody').on('click', '.button-delete', function(){
@@ -283,6 +274,44 @@
                 }
             });
         });
+
+        function fallbackCopyTextToClipboard(text) {
+            var textArea = document.createElement("textarea");
+            textArea.value = text;
+
+            // Avoid scrolling to bottom
+            textArea.style.top = "0";
+            textArea.style.left = "0";
+            textArea.style.position = "fixed";
+
+            document.body.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
+
+            try {
+                var successful = document.execCommand('copy');
+                var msg = successful ? 'successful' : 'unsuccessful';
+                console.log('Fallback: Copying text command was ' + msg);
+            } catch (err) {
+                console.error('Fallback: Oops, unable to copy', err);
+            }
+
+            document.body.removeChild(textArea);
+        }
+
+        function copyTextToClipboard(text) {
+            if (!navigator.clipboard) {
+                fallbackCopyTextToClipboard(text);
+                return;
+            }
+            navigator.clipboard.writeText(text).then(function() {
+                /* clipboard successfully set */
+                window.alert('Success! The text was copied to your clipboard')
+            }, function(err) {
+                /* clipboard write failed */
+                window.alert('Opps! Your browser does not support the Clipboard')
+            });
+        }
     });
 </script>
 @endpush
